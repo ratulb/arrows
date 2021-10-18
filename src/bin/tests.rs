4f95370e495382d165e335490c1a82c75236748f
Ratul Buragohain <ratul75@hotmail.com>
2021-10-18 02:47:57 +0000

    WIP

--- src/bin/tests.rs
+++ src/bin/tests.rs
@@ -10,6 +10,7 @@ pub async fn main() {
     write_addr_test1().await;
     //create_actor_builder_test_cloning().await;
     send_complex_msg_test_1().await;
+    send_msg_within_msg_test_1().await;
 }
 
 async fn actor_test_with_closure() {
@@ -147,4 +148,71 @@ async fn send_complex_msg_test_1() {
 
     let mut actor_builder = ActorBuilder;
     actor_builder.receive(msg);
+}
+async fn send_msg_within_msg_test_1() {
+    #[derive(PartialEq, Clone, Debug, Serialize, Deserialize)]
+    struct Complex<T> {
+        inner: T,
+        elems: Vec<Simple>,
+    }
+    #[derive(PartialEq, Clone, Debug, Serialize, Deserialize)]
+    struct Inner {
+        name: String,
+        children: Vec<String>,
+        male: bool,
+        age: u8,
+    }
+    #[derive(PartialEq, Clone, Debug, Serialize, Deserialize)]
+    struct Simple {
+        e1: i32,
+        e2: usize,
+        e3: Option<bool>,
+    }
+    let simple = Simple {
+        e1: 42,
+        e2: 999,
+        e3: Some(false),
+    };
+
+    let inner = Inner {
+        name: "Some body".to_string(),
+        children: vec!["Some value".to_string()],
+        male: true,
+        age: 99,
+    };
+
+    let complex = Complex {
+        inner,
+        elems: vec![simple],
+    };
+    let complex_as_opt = option_of_bytes(&complex);
+    let msg = Message::new(complex_as_opt , "addr_from", "addr_to");
+    let msg_container = Message::new(option_of_bytes(&msg),"addr_from", "addr_to");
+
+    struct NewActor;
+
+    impl Actor for NewActor {
+      fn receive(&mut self, msg: Message) -> Option<Message> {
+         println!("New actor received msg ->");           
+         println!("");
+         println!("{:?}", msg);
+         let mut msg = msg;
+         let inner_msg_option: Option<Vec<u8>> = msg.get_content_out();
+         let inner_vec = inner_msg_option.unwrap();
+         let mut inner_msg: Message = from_bytes(&inner_vec).ok().unwrap();
+         let inner_content_option = inner_msg.get_content_out();
+         let bytes_for_complex = inner_content_option.unwrap();
+         let nested_complex: Complex<Inner> = from_bytes(&bytes_for_complex).ok().unwrap();
+
+         println!("The nested complex: {:?}", nested_complex);
+         let returned_complex_bytes = option_of_bytes(&nested_complex);
+         let returned_msg  = Message::new(returned_complex_bytes, "addr_from", "addr_to");
+         Some(returned_msg)
+      }
+    }
+    let mut new_actor = NewActor;
+    let call_result = new_actor.receive(msg_container);
+    let call_result = call_result.unwrap().get_content_out().unwrap();
+    let result_complex: Complex<Inner> = from_bytes(&call_result).ok().unwrap();
+    assert_eq!(result_complex, complex);
 }
