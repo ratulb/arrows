09f1950a6f9a35ac924c4e9dc95a4c366bbc2da2
Ratul Buragohain <ratul75@hotmail.com>
2021-12-08 23:02:22 +0000

    WIP

--- src/sender.rs
+++ src/sender.rs
@@ -4,64 +4,34 @@ use std::collections::HashMap;
 #[macro_export]
 macro_rules! send {
     ($($actor_name:literal, ($($msg:expr),*)),*)  => {
-        let mut actor_msgs = HashMap::new();
-            $(
-                let addr = $crate::Addr::new($actor_name);
-                let size = $crate::send![@SIZE; $($msg),*];
-                let msgs = actor_msgs.entry(&addr)
-                                     .or_insert(Vec::with_capacity(size));
-                $(
-                    let msg: $crate::Msg = $msg;
-                    msgs.push(msg);
-                )*
-            )*
-        $crate::recv(actor_msgs);
+        $crate::send!(@DELEGATE; $($crate::send!(@TO_ADDR; $actor_name), ($($msg),*)),*);
     };
 
     ($($actor_name:literal, $($msg:expr),*),*) => {
-        let mut actor_msgs = HashMap::new();
-            $(
-                let addr = $crate::Addr::new($actor_name);
-                let size = $crate::send![@SIZE; $($msg),*];
-                let msgs = actor_msgs.entry(&addr)
-                                     .or_insert(Vec::with_capacity(size));
-
-                $(
-                    let msg: $crate::Msg = $msg;
-                    msgs.push(msg);
-                )*
-            )*
-        $crate::recv(actor_msgs);
+        $crate::send!(@DELEGATE; $($crate::send!(@TO_ADDR; $actor_name), ($($msg),*)),*);
     };
 
     ($($addr:expr, ($($msg:expr),*)),*)  => {
-        let mut actor_msgs = HashMap::new();
-            $(
-                let addr: $crate::Addr = $addr;
-                let size = $crate::send![@SIZE; $($msg),*];
-                let msgs = actor_msgs.entry(&addr)
-                                     .or_insert(Vec::with_capacity(size));
-                $(
-                    let msg: $crate::Msg = $msg;
-                    msgs.push(msg);
-                )*
-            )*
-        $crate::recv(actor_msgs);
+        $crate::send!(@DELEGATE; $($addr, ($($msg),*)),*);
     };
 
     ($($addr:expr, $($msg:expr),*),*) => {
+        $crate::send!(@DELEGATE; $($addr, ($($msg),*)),*);
+    };
+
+    (@DELEGATE; $($addr:expr, ($($msg:expr),*)),*) => {
         let mut actor_msgs = HashMap::new();
             $(
                 let addr: $crate::Addr = $addr;
                 let size = $crate::send![@SIZE; $($msg),*];
                 let msgs = actor_msgs.entry(&addr)
-                                     .or_insert(Vec::with_capacity(size));
-                $(
-                    let msg: $crate::Msg = $msg;
-                    msgs.push(msg);
-                )*
+                    .or_insert(Vec::with_capacity(size));
+                    $(
+                        let msg: $crate::Msg = $msg;
+                        msgs.push(msg);
+                    )*
             )*
-        $crate::recv(actor_msgs);
+                $crate::recv(actor_msgs);
     };
 
     (@SIZE; $($msg:expr),*) => {
@@ -69,6 +39,10 @@ macro_rules! send {
     };
 
     (@SUB; $_msg:expr) => {()};
+
+    (@TO_ADDR; $actor_name:literal) => {
+        $crate::Addr::new($actor_name)
+    };
 }
 
 pub(crate) fn recv(msgs: HashMap<&Addr, Vec<Msg>>) {
